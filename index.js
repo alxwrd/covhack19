b0da89b169c1d071dd44c81792076a773951999e
Alex Ward <alxwrd@googlemail.com>
2019-03-18 14:27:00 +0000

    clean up index.js

--- index.js
+++ index.js
@@ -38,72 +38,18 @@ async function init() {
 }
 
 
-// function loop() {
-//
-//
-//   while (true) {
-//     // The dataset object where we will store activations.
-//     let controllerDataset = new ControllerDataset(NUM_CLASSES);
-//
-//     // Fetch data from python app
-//     let data = { "Scenario": "This is a test", "Options": { "a": "apple", "b": "banana", "c": "cherry" } }
-//
-//     // For each option
-//
-//
-//       document.getElementById("image").src = `images/${key}.png`
-//
-//       for (let x=1; x<30; x++) {
-//         controllerDataset.addExample(webcam.capture(), 0)
-//       }
-//
-//     })
-//     break;
-//     // Display image, and train
-//
-//     // Use the data to populate text on the screen
-//
-//     // User completes scenario
-//   }
-// }
-
-// type writer effect
-
-
 async function loop(data) {
   let controllerDataset = new ControllerDataset(NUM_CLASSES);
-
-  // Fetch data from python app below is test case
-  //let data = { "Scenario": "This is a test", "Options": { "a": "apple", "b": "banana", "c": "cherry" } }
-
-  // will be un commented in future
-  // let data = read();
-  // console.log(read())
-
-
-  // let data = { "Scenario": "This is a test", "Options": { "e": "apple", "f": "banana", "g": "cherry" } }
-  // let data = { "Scenario": "An evil dragon stands in your way. What will you offer it: A Sausage, a Bucket or a Joke?", "Options": { "s": "Sausage", "b": "Bucket", "j": "Joke" } };
 
   console.log(data)
 
   var label = 0;
   var key = Object.keys(data.Options)[label];
   console.log(key)
-  // console.log(data.Options[Object.keys(data.Options)[0]])
 
-  document.getElementById("image").src = `http://127.0.0.1:5000/images/${key}.png`
+  document.getElementById("image").src = `images/${key}.png`
   document.getElementById("letter").innerHTML = key;
 
-
-  // Warm up the model. This uploads weights to the GPU and compiles the WebGL
-  // programs so the first time we collect data from the webcam it will be
-  // quick.
-  // tf.tidy(() => truncatedMobileNet.predict(webcam.capture()));
-  window.onload = function () {
-    typeWriter();// code goes here
-  };
-
-  //  master
 
 
   document.getElementById("train").addEventListener("click", () => {
@@ -119,7 +65,7 @@ async function loop(data) {
       label++;
       var key = Object.keys(data.Options)[label];
       console.log(key)
-      document.getElementById("image").src = `http://127.0.0.1:5000/images/${key}.png`
+      document.getElementById("image").src = `images/${key}.png`
       document.getElementById("letter").innerHTML = key;
       return;
     }
@@ -129,18 +75,8 @@ async function loop(data) {
 
       console.log("starting story ...")
 
-      var i = 0;
-      var speed = 50;
-      var txt = data.Scenario
 
-      function typeWriter() {
-        if (i < txt.length) {
-          document.getElementById("scenario").innerHTML += txt.charAt(i);
-          i++;
-          setTimeout(typeWriter, speed);
-        }
-      }
-      typeWriter();
+      typeWriter(data.Scenario);
 
 
       // can't figure out why it's predicitng before training
@@ -150,7 +86,7 @@ async function loop(data) {
         let img = webcam.capture();
         controllerDataset.predict(img).then(function (result) {
           let key = Object.keys(data.Options)[result];
-          document.getElementById("image").src = `http://127.0.0.1:5000/images/${key}.png`
+          document.getElementById("image").src = `images/${key}.png`
           document.getElementById("letter").innerHTML = key;
           document.getElementById("choice").innerText = data.Options[key];
         });
@@ -164,20 +100,8 @@ async function loop(data) {
         let img = webcam.capture();
         controllerDataset.predict(img).then(function (result) {
           let key = Object.keys(data.Options)[result];
-          var i = 0;
-          var speed = 50;
-          var txt = data[key]
 
-          document.getElementById("scenario").innerHTML = "";
-
-          function typeWriter() {
-            if (i < txt.length) {
-              document.getElementById("scenario").innerHTML += txt.charAt(i);
-              i++;
-              setTimeout(typeWriter, speed);
-            }
-          }
-          typeWriter();
+          typeWriter(data[key]);
         });
 
       })
@@ -185,98 +109,38 @@ async function loop(data) {
   })
 }
 
-// background experimental
-
-// Some random colors
-// const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-const colors = ["#1B1B1B", "#F4F1D6"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-
-  balls.push(ball);
-  document.body.append(ball);
-}
-
-// <<<<<<< hacky-no-for-loop
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11) / 10,
-    y: Math.random() * 12 / 10
-  };
-
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
-});
-
 
 document.getElementById("start").addEventListener("click", () => {
-  fetchData();
+  fetch("options.json").then((response) => {
+    return response.json()
+  })
+    .then((data) => {
+      let scenario = data[Math.floor(Math.random() * data.length)];
+
+      loop(scenario);
+
+    })
 })
 
 
-function fetchData() {
-  fetch("http://127.0.0.1:5000/options").then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    loop(data);
-  })
-}
+function typeWriter(text, pos) {
+  let speed = Math.floor(Math.random() * 200);
 
+  if (!pos) {
+    pos = 0;
+  }
 
+  if (pos === 0) {
+    document.getElementById("scenario").innerHTML = "";
+  }
 
-// type writer effect
-var i = 0;
-var speed = 50;
-var txt = "This is a scenario. You have been presented with an [a]pple, [b]annana, and a [c]herry. Which do you choose?"
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("scenario").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  if (pos < text.length) {
+    document.getElementById("scenario").innerHTML += text.charAt(pos);
+    pos++;
+    setTimeout(() => {
+      typeWriter(text, pos)
+    }, speed);
   }
 }
 
-function read() {
-  $.ajax({
-    url: 'http://127.0.0.1:5000/options ',
-    data: $('form').serialize(),
-    type: 'POST',
-    success: function (response) {
-      var current = JSON.parse(response);
-      console.log(current);
-      return current
-    },
-    error: function (error) {
-      console.log(error);
-    }
-  });
-}
-
-// master
-// Initialize the application.
 init();
