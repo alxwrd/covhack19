5593243ab387845fd83a6a55ac3a9723cf0d80d0
Alex Ward <alxwrd@googlemail.com>
2019-03-19 17:44:46 +0000

    redo header

--- src/index.js
+++ src/index.js
@@ -131,6 +131,7 @@ function main(data) {
 
 
 document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("header").classList.add("header-hidden")
   reset();
 
   fetch("options.json").then((response) => {
